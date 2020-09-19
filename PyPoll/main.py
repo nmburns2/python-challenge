7877d23eef044a5daf3fd90b776b5f1fc12dc6f8
Nicholas Burns <nmburns@triad.rr.com>
2020-09-19 16:42:19 -0400

    add data file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,12 +4,11 @@ import os
 path = os.path.join('Resources', 'election_data.csv')
    
 votes = []
-county = []
 candidates = []
 khan = []
-correy = []
 li = []
 otooley = []
+correy = []
 
 with open (path) as csvfile:
      reader = csv.reader(csvfile, delimiter= ',')
@@ -19,8 +18,11 @@ with open (path) as csvfile:
 
 total = (len(votes))
 
+print("_______________________")
+print("Election Results:")
+print("-----------------------")
 print(f"Total Votes: {total}")
-print("-----------------")
+print("-----------------------")
 
 for y in candidates:
     if y == "Khan":
@@ -37,12 +39,30 @@ otooleys_votes = len(otooley)
 lis_votes = len(li)
 correys_votes = len (correy)
 
-print(f"Khan's Votes: {khans_votes}")
-print("-----------------")
-print(f"O'Tooley's Votes: {otooleys_votes}")
-print("-----------------")
-print(f"Li's Votes: {lis_votes}")
-print("-----------------")
-print(f"Correy's Votes: {correys_votes}")
+khans_percent = ((khans_votes/total) * 100)
+otooleys_percent = ((otooleys_votes/total) * 100)
+lis_percent = ((lis_votes/total) * 100)
+correys_percent = ((correys_votes/total) * 100)
+
+print(f"Khan's Votes: {khans_votes}, {khans_percent}%")
+print(f"O'Tooley's Votes: {otooleys_votes}, {otooleys_percent}%")
+print(f"Li's Votes: {lis_votes}, {lis_percent}%")
+print(f"Correy's Votes: {correys_votes}, {correys_percent}%")
+
+if khans_votes > max(lis_votes, otooleys_votes, correys_votes):
+    winner = "Khan"
+if lis_votes > max(khans_votes, otooleys_votes, correys_votes):
+    winner = "Li"
+if otooleys_votes > max(khans_votes, correys_votes, lis_votes):
+    winner = "O'Tooley"
+if correys_votes > max(khans_votes, otooleys_votes, lis_votes):
+    winner = "Correy"
+
+
+print("-----------------------")
+print(f'Winner: {winner}')
+print("_______________________")
+
+
 
 
