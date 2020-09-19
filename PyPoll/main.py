d81ce81f6f0721bb7d98dbea06edc5a33fe8e150
Nicholas Burns <nmburns@triad.rr.com>
2020-09-19 14:54:17 -0400

    add data file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,8 +4,21 @@ import os
 csv_path = os.path.join('Resources', 'election_data.csv')
 
 with open(csv_path) as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
+    csvreader = csv.reader(csvfile, delimiter= ',')
 
-print(reader)
+votes = []
+county = []
+candidates = []
+khan = []
+correy = []
+li = []
+otooley = []
 
+for x in csvreader:
+    votes.append(int(x[0]))
+    county.append(int(x[1]))
+    candidates.append(int(x[2]))
 
+total = (len(votes))
+
+print(total)
