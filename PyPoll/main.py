8bbe8ccedc8a005dffd7433b920fdca12bb7738a
Nicholas Burns <nmburns@triad.rr.com>
2020-09-19 16:00:39 -0400

    add data file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -21,3 +21,28 @@ with open (path) as csvfile:
 total = (len(votes))
 
 print(total)
+print("-----------------")
+
+for y in candidates:
+    if y == "Khan":
+        khan.append(candidates)
+    if y == "O'Tooley":
+        otooley.append(candidates)
+    if y == 'Li':
+        li.append(candidates)
+    if y == 'Correy':
+        correy.append(candidates)
+
+khans_votes = len(khan)
+otooleys_votes = len(otooley)
+lis_votes = len(li)
+correys_votes = len (correy)
+
+print(khans_votes)
+print("-----------------")
+print(lis_votes)
+print("-----------------")
+print (otooleys_votes)
+print("-----------------")
+print(correys_votes)
+
